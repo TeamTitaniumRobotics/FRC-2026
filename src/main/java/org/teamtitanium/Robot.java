40aacf97e4d8f465e9c26fd62f9a38f8c1c24428
Oliver Weller <ImpactTang@outlook.com>
2025-12-30 23:38:31 -0600

    Setup AdvantageKit and renamed file tree

--- src/main/java/org/teamtitanium/Robot.java
+++ src/main/java/org/teamtitanium/Robot.java
@@ -2,13 +2,14 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package org.teamtitanium;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import org.littletonrobotics.junction.LoggedRobot;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
