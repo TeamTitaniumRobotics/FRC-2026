6fd4c260e1f1d2ae60178c76a319d38bd276fd95
Oliver W <ImpactTang@outlook.com>
2026-01-01 22:27:26 -0600

    Setup PhoenixOdometryThread

--- src/main/java/org/teamtitanium/BuildConstants.java
+++ src/main/java/org/teamtitanium/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "5175bd50f7a161d076f1b85bf6cca0fa35171b89";
-  public static final String GIT_DATE = "2025-12-30 23:49:13 CST";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "920b5ebdf566ae4ed9c569704caaf87e385134a1";
+  public static final String GIT_DATE = "2026-01-01 14:15:48 CST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-12-30 23:55:50 CST";
-  public static final long BUILD_UNIX_TIME = 1767160550493L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-01 22:14:25 CST";
+  public static final long BUILD_UNIX_TIME = 1767327265015L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
--- src/main/java/org/teamtitanium/subsystems/swerve/Swerve.java
+++ src/main/java/org/teamtitanium/subsystems/swerve/Swerve.java
@@ -1,3 +1,13 @@
 package org.teamtitanium.subsystems.swerve;
 
-public class Swerve {}
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.teamtitanium.utils.TunerConstants;
+
+import com.ctre.phoenix6.CANBus;
+
+public class Swerve {
+    public static final double ODOMETRY_FREQUENCY = new CANBus(TunerConstants.DrivetrainConstants.CANBusName).isNetworkFD() ? 250.0 : 100.0;
+    public static final Lock odometryLock = new ReentrantLock();
+}
