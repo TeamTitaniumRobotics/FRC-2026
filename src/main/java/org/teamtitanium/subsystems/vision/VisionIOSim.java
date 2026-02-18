5fb1f5036ac5ba8174fa8d94f50b8f2cf51a5a09
Aiden Burgstahler <158793389+TheBraggerBear@users.noreply.github.com>
2026-02-18 02:18:34 -0600

    Format.

--- src/main/java/org/teamtitanium/subsystems/vision/VisionIOSim.java
+++ src/main/java/org/teamtitanium/subsystems/vision/VisionIOSim.java
@@ -1,7 +1,6 @@
 package org.teamtitanium.subsystems.vision;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.Timer;
 import java.util.Optional;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
